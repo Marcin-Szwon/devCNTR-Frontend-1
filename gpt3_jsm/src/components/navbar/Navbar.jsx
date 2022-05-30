c162bb29e592a2f128893d09423b7aa5f61f3c9d
unknown <mko2001@op.pl>
2022-05-30 15:04:20 +0200

    first commit

---
+++ gpt3_jsm/src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
