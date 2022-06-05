f63504a0f03016ac31e48dad753be6e1ea73b8bf
unknown <mko2001@op.pl>
2022-06-05 23:05:56 +0200

    add p tags with menu, p tag with sign in and button sign up

--- gpt3_jsm/src/components/navbar/Navbar.jsx
+++ gpt3_jsm/src/components/navbar/Navbar.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
 
 function Navbar() {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo" />
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
       </div>
     </div>
   );
