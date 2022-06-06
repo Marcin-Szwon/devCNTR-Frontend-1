8780a91ae35883699461447f23d100b4e3275d20
unknown <mko2001@op.pl>
2022-06-06 23:10:07 +0200

    create a function with props

--- gpt3_jsm/src/components/feature/Feature.jsx
+++ gpt3_jsm/src/components/feature/Feature.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./feature.css";
 
-function Feature() {
-  return <div>Feature</div>;
+// eslint-disable-next-line react/prop-types
+function Feature({ title, text }) {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Feature;
